434956fee57abe964527e87dbb4e2c5fa2ae5496
Kumar-Alok-Anand <151454591+Kumar-Alok-Anand@users.noreply.github.com>
2025-09-16 20:51:45 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+let btn= document.querySelector(".btn");
+btn.addEventListener("click",( )=>{
+    // btn.innerHTML="you have changed ";
+    document.querySelector(".g_son").innerHTML="I have changed by clicking !!"
+})
